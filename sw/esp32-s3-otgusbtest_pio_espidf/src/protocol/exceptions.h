3124050cf03c1acfab01b57502c213210aa38ce2
Pavel Kolský <kolskpa1@fit.cvut.cz>
2025-04-22 23:26:39 +0200

    File cleanup, restruct

--- sw/esp32-s3-otgusbtest_pio_espidf/src/protocol/exceptions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdexcept>
-
-class SocketException : public std::runtime_error
-{
-public:
-    SocketException(const std::string &cause) : std::runtime_error(cause) {}
-};
